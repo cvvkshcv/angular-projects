c743b1d614958b820dcd337e08c9babdfdf8f6a4
Vikash UI_GEMS <cvvkshcv@gmail.com>
2021-10-24 19:38:33 +0530

    removed material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatCardModule } from '@angular/material/card';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatInputModule } from '@angular/material/input';
-
 @NgModule({
   declarations: [
     AppComponent
@@ -18,12 +11,6 @@ import { MatInputModule } from '@angular/material/input';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatProgressBarModule,
-    MatCardModule,
-    MatChipsModule,
-    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
